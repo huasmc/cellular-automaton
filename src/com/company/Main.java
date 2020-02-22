e1f96e72b9049b7c74d0ab486005d30401bead9e
Huascar <huascarmc@hotmail.com>
2020-02-22 15:53:42 -0400

    added main code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Board board = new Board();
+        int generation = 10;
+
+        for(int gen=0; gen < generation; gen++) {
+            int[][] newBoard = BoardGenerator.generateBoard(board.getBoard(), board.getBoard().length, board.getBoard()[0].length);
+            board.setBoard(newBoard);
+            System.out.println(Arrays.deepToString(board.getBoard())
+                    .replace("0", " ")
+                    .replace("1", "X")
+                    .replace("], ", "]\n")
+                    .replaceAll("[\\[\\] ,]", " "));
+            generation++;
+        }
     }
 }
